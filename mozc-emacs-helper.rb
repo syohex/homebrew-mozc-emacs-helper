561eda89a1b47ba5ece6b8a04532e772397e2824
Syohei YOSHIDA <syohex@gmail.com>
2015-08-09 19:36:38 +0900

    Don't change directory

--- mozc-emacs-helper.rb
+++ mozc-emacs-helper.rb
@@ -9,9 +9,9 @@ class MozcEmacsHelper < Formula
     system "git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git"
     system "./depot_tools/gclient config https://github.com/google/mozc.git"
     system "./depot_tools/gclient sync"
-    system "cd mozc && patch src/build_mozc.py < ../build_mozc.py.patch"
-    system "cd mozc && python src/build_mozc.py gyp --noqt"
-    system "cd mozc && python src/build_mozc.py build -c Release src/mac/mac.gyp:GoogleJapaneseInput src/mac/mac.gyp:gen_launchd_confs src/unix/emacs/emacs.gyp:mozc_emacs_helper"
+    system "patch mozc/src/build_mozc.py < build_mozc.py.patch"
+    system "python mozc/src/build_mozc.py gyp --noqt"
+    system "python mozc/src/build_mozc.py build -c Release mozc/src/mac/mac.gyp:GoogleJapaneseInput mozc/src/mac/mac.gyp:gen_launchd_confs mozc/src/unix/emacs/emacs.gyp:mozc_emacs_helper"
 
     bin.install 'mozc/src/out_mac/Release/mozc_emacs_helper'
     system 'sudo cp -r mozc/src/out_mac/Release/Mozc.app /Library/Input\ Methods/'
