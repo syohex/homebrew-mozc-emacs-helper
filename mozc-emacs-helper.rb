23f0746e2b3b233c194949653ee3498f464fc799
Syohei YOSHIDA <syohex@gmail.com>
2015-08-09 19:22:33 +0900

    chdir to mozc/

--- mozc-emacs-helper.rb
+++ mozc-emacs-helper.rb
@@ -9,14 +9,14 @@ class MozcEmacsHelper < Formula
     system "git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git"
     system "./depot_tools/gclient config https://github.com/google/mozc.git"
     system "./depot_tools/gclient sync"
-    system "patch src/build_mozc.py < build_mozc.py.patch"
-    system "python src/build_mozc.py gyp --noqt"
-    system "python src/build_mozc.py build -c Release src/mac/mac.gyp:GoogleJapaneseInput src/mac/mac.gyp:gen_launchd_confs src/unix/emacs/emacs.gyp:mozc_emacs_helper"
+    system "cd mozc && patch src/build_mozc.py < build_mozc.py.patch"
+    system "cd mozc && python src/build_mozc.py gyp --noqt"
+    system "cd mozc && python src/build_mozc.py build -c Release src/mac/mac.gyp:GoogleJapaneseInput src/mac/mac.gyp:gen_launchd_confs src/unix/emacs/emacs.gyp:mozc_emacs_helper"
 
-    bin.install 'src/out_mac/Release/mozc_emacs_helper'
-    system 'sudo cp -r src/out_mac/Release/Mozc.app /Library/Input\ Methods/'
-    system 'sudo cp src/out_mac/DerivedSources/Release/mac/org.mozc.inputmethod.Japanese.Converter.plist /Library/LaunchAgents'
-    system 'sudo cp src/out_mac/DerivedSources/Release/mac/org.mozc.inputmethod.Japanese.Renderer.plist /Library/LaunchAgents'
+    bin.install 'mozc/src/out_mac/Release/mozc_emacs_helper'
+    system 'sudo cp -r mozc/src/out_mac/Release/Mozc.app /Library/Input\ Methods/'
+    system 'sudo cp mozc/src/out_mac/DerivedSources/Release/mac/org.mozc.inputmethod.Japanese.Converter.plist /Library/LaunchAgents'
+    system 'sudo cp mozc/src/out_mac/DerivedSources/Release/mac/org.mozc.inputmethod.Japanese.Renderer.plist /Library/LaunchAgents'
   end
 
   def caveats
